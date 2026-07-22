7afada76fad8c593a5db18d9cd3faced5684d17f
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-22 13:38:40 +0330

    Update admin.py

--- admin.py
+++ admin.py
@@ -9,54 +9,76 @@ def admin_buttons(order_id):
     keyboard = [
 
         [
+
             InlineKeyboardButton(
+
                 "✅ تأیید پرداخت",
+
                 callback_data=f"approve_{order_id}"
+
             )
+
         ],
 
         [
+
             InlineKeyboardButton(
+
                 "❌ رد پرداخت",
+
                 callback_data=f"reject_{order_id}"
+
             )
+
         ]
 
     ]
+
 
     return InlineKeyboardMarkup(keyboard)
 
 
 
-def create_subscription():
+
+def create_subscription(volume):
+
 
     marzban = Marzban()
 
 
+
     user = marzban.create_user(
+
         username=None,
-        days=30
+
+        days=30,
+
+        data_limit=volume * 1024 * 1024 * 1024
+
     )
+
 
 
     if user:
 
-        username = user.get(
-            "username"
-        )
-
-
-        sub = (
-            user.get(
-                "subscription_url"
-            )
-        )
-
 
         return {
-            "username": username,
-            "subscription": sub
+
+            "username": user.get(
+
+                "username"
+
+            ),
+
+
+            "subscription": user.get(
+
+                "subscription_url"
+
+            )
+
         }
+
 
 
     return None
