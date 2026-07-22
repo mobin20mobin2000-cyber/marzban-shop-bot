3a4626abcc100fda1b537e08c039902b1eb3c539
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-22 13:24:16 +0330

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,8 @@ from payment import get_payment_text
 from admin import admin_buttons, create_subscription
 
 
-
+# نگهداری سفارش‌های در انتظار
 waiting_receipt = {}
-
 
 
 def user_menu():
@@ -73,6 +72,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     user_id = update.effective_user.id
 
+
     if user_id == ADMIN_ID:
 
         await update.message.reply_text(
@@ -83,8 +83,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
 
         await update.message.reply_text(
-            "🤖 به ربات فروش خوش آمدید\n\n"
-            "انتخاب کنید:",
+            "🤖 ربات فروش اشتراک\n\n"
+            "یک گزینه را انتخاب کنید:",
             reply_markup=user_menu()
         )
 
@@ -96,12 +96,14 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await query.answer()
 
-
     user_id = query.from_user.id
 
 
 
+    # خرید
+
     if query.data == "buy":
+
 
         order_id = create_order(
             user_id,
@@ -118,21 +120,29 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
+    # سرویس من
+
     elif query.data == "my_service":
 
+
         await query.message.reply_text(
-            "📦 هنوز سرویسی ندارید."
+            "📦 هنوز سرویسی برای شما ثبت نشده است."
         )
 
 
 
+    # پشتیبانی
+
     elif query.data == "support":
+
 
         await query.message.reply_text(
             "🆘 پشتیبانی"
         )
 
 
+
+    # تایید مدیر
 
     elif query.data.startswith("approve_"):
 
@@ -143,42 +153,102 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
 
+        customer_id = None
+
+
+        for uid, oid in waiting_receipt.items():
+
+            if oid == order_id:
+
+                customer_id = uid
+
+                break
+
+
+
+        if customer_id is None:
+
+
+            await query.message.reply_text(
+                "❌ کاربر سفارش پیدا نشد"
+            )
+
+            return
+
+
+
         result = create_subscription()
+
 
 
         if result:
 
 
             sub_url = (
+
                 MARZBAN_URL.rstrip("/")
+
                 +
+
                 result["subscription"]
+
             )
 
 
+
+            await context.bot.send_message(
+
+                chat_id=customer_id,
+
+                text=(
+
+                    "✅ پرداخت شما تأیید شد\n\n"
+
+                    f"👤 نام کاربر:\n"
+
+                    f"{result['username']}\n\n"
+
+                    "🔗 لینک سابسکریپشن:\n"
+
+                    f"{sub_url}"
+
+                )
+
+            )
+
+
+
             await query.message.reply_text(
-                "✅ پرداخت تأیید شد\n\n"
-                f"👤 کاربر:\n"
-                f"{result['username']}\n\n"
-                "🔗 لینک اشتراک:\n"
-                f"{sub_url}"
+
+                "✅ پرداخت تأیید شد\n"
+
+                "📩 لینک برای مشتری ارسال شد."
+
             )
 
 
         else:
 
+
             await query.message.reply_text(
-                "❌ ساخت اشتراک خطا داشت"
+
+                "❌ ساخت اشتراک ناموفق بود"
+
             )
 
 
+
+    # رد پرداخت
 
     elif query.data.startswith("reject_"):
 
 
         await query.message.reply_text(
+
             "❌ پرداخت رد شد"
+
         )
+
 
 
 
@@ -195,10 +265,12 @@ async def receipt_photo(
         return
 
 
+
     order_id = waiting_receipt[user_id]
 
 
     photo = update.message.photo[-1]
+
 
 
     await context.bot.send_photo(
@@ -208,9 +280,13 @@ async def receipt_photo(
         photo=photo.file_id,
 
         caption=(
+
             "📥 رسید پرداخت جدید\n\n"
+
             f"👤 کاربر:\n{user_id}\n\n"
+
             f"🆔 سفارش:\n{order_id}"
+
         ),
 
         reply_markup=admin_buttons(order_id)
@@ -218,10 +294,15 @@ async def receipt_photo(
     )
 
 
+
     await update.message.reply_text(
+
         "✅ رسید ارسال شد\n"
+
         "⏳ منتظر تأیید مدیر باشید."
+
     )
+
 
 
 
@@ -229,38 +310,63 @@ def main():
 
 
     app = (
+
         Application
+
         .builder()
+
         .token(BOT_TOKEN)
+
         .build()
+
     )
 
 
+
     app.add_handler(
+
         CommandHandler(
+
             "start",
+
             start
+
         )
+
     )
 
 
+
     app.add_handler(
+
         CallbackQueryHandler(
+
             button
+
         )
+
     )
+
 
 
     app.add_handler(
+
         MessageHandler(
+
             filters.PHOTO,
+
             receipt_photo
+
         )
+
     )
+
 
 
     print(
+
         "Bot Started ✅"
+
     )
 
 
