eb84a4d997a7202cc1f44e4d0be6e5685f52e9a4
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-22 12:45:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -18,23 +18,73 @@ async def start(
 
     await update.message.reply_text(
         "🤖 ربات فروش Marzban فعال شد\n\n"
-        "به زودی سیستم فروش اشتراک آماده می‌شود."
+        "🛒 سیستم فروش به زودی آماده می‌شود."
     )
+
+
+
+async def test_user(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    await update.message.reply_text(
+        "⏳ در حال ساخت کاربر..."
+    )
+
+
+    marzban = Marzban()
+
+
+    result = marzban.create_user(
+        username=None,
+        days=30
+    )
+
+
+    if result:
+
+        username = result.get(
+            "username",
+            "نامشخص"
+        )
+
+
+        await update.message.reply_text(
+            f"✅ کاربر ساخته شد\n\n"
+            f"👤 نام کاربر:\n"
+            f"{username}\n\n"
+            f"{result}"
+        )
+
+    else:
+
+        await update.message.reply_text(
+            "❌ ساخت کاربر ناموفق بود"
+        )
 
 
 
 def main():
 
+
     print("Testing Marzban...")
 
+
     marzban = Marzban()
+
 
     marzban.test()
 
 
-    app = Application.builder() \
-        .token(BOT_TOKEN) \
+
+    app = (
+        Application
+        .builder()
+        .token(BOT_TOKEN)
         .build()
+    )
+
 
 
     app.add_handler(
@@ -45,7 +95,18 @@ def main():
     )
 
 
-    print("Bot Started ✅")
+    app.add_handler(
+        CommandHandler(
+            "testuser",
+            test_user
+        )
+    )
+
+
+
+    print(
+        "Bot Started ✅"
+    )
 
 
     app.run_polling()
@@ -53,4 +114,5 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
