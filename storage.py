05a8c65dbac850cef2014dfe358b6416bd2e8a03
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-22 15:25:30 +0330

    Update storage.py

--- storage.py
+++ storage.py
@@ -2,19 +2,27 @@ import json
 import os
 
 
-FILE = "users.json"
+USERS_FILE = "users.json"
+
+ORDERS_FILE = "orders.json"
 
 
+
+
+
+# =========================
+# کاربران
+# =========================
 
 def load_users():
 
-    if not os.path.exists(FILE):
+    if not os.path.exists(USERS_FILE):
 
         return {}
 
 
     with open(
-        FILE,
+        USERS_FILE,
         "r",
         encoding="utf-8"
     ) as f:
@@ -23,10 +31,12 @@ def load_users():
 
 
 
+
+
 def save_users(users):
 
     with open(
-        FILE,
+        USERS_FILE,
         "w",
         encoding="utf-8"
     ) as f:
@@ -37,6 +47,8 @@ def save_users(users):
             ensure_ascii=False,
             indent=4
         )
+
+
 
 
 
@@ -62,6 +74,8 @@ def save_service(
 
 
 
+
+
 def get_service(user_id):
 
     users = load_users()
@@ -70,3 +84,116 @@ def get_service(user_id):
     return users.get(
         str(user_id)
     )
+
+
+
+
+
+# =========================
+# سفارش ها
+# =========================
+
+def load_orders():
+
+    if not os.path.exists(ORDERS_FILE):
+
+        return {}
+
+
+    with open(
+        ORDERS_FILE,
+        "r",
+        encoding="utf-8"
+    ) as f:
+
+        return json.load(f)
+
+
+
+
+
+def save_orders(orders):
+
+    with open(
+        ORDERS_FILE,
+        "w",
+        encoding="utf-8"
+    ) as f:
+
+        json.dump(
+            orders,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
+
+
+
+
+
+def save_order(
+    order_id,
+    user_id,
+    plan
+):
+
+    orders = load_orders()
+
+
+    orders[str(order_id)] = {
+
+        "order_id": order_id,
+
+        "user_id": user_id,
+
+        "plan": plan
+
+    }
+
+
+    save_orders(orders)
+
+
+
+
+
+def get_order(value):
+
+    orders = load_orders()
+
+
+    # پیدا کردن با شماره سفارش
+
+    if str(value) in orders:
+
+        return orders[str(value)]
+
+
+
+    # پیدا کردن با آیدی کاربر
+
+    for order in orders.values():
+
+        if order["user_id"] == value:
+
+            return order
+
+
+
+    return None
+
+
+
+
+
+def delete_order(order_id):
+
+    orders = load_orders()
+
+
+    if str(order_id) in orders:
+
+        del orders[str(order_id)]
+
+
+    save_orders(orders)
