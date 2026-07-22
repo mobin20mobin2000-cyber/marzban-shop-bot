5181d8273c7195965aa82b4e2f04ab8c4c4a162e
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-22 15:51:59 +0330

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,199 +1,56 @@
-import json
-import os
-
-
-USERS_FILE = "users.json"
-
-ORDERS_FILE = "orders.json"
-
-
-
-
-
 # =========================
-# کاربران
+# سرویس کاربران
 # =========================
 
-def load_users():
+services = {}
 
-    if not os.path.exists(USERS_FILE):
+def save_service(user_id, username, subscription):
 
-        return {}
-
-
-    with open(
-        USERS_FILE,
-        "r",
-        encoding="utf-8"
-    ) as f:
-
-        return json.load(f)
-
-
-
-
-
-def save_users(users):
-
-    with open(
-        USERS_FILE,
-        "w",
-        encoding="utf-8"
-    ) as f:
-
-        json.dump(
-            users,
-            f,
-            ensure_ascii=False,
-            indent=4
-        )
-
-
-
-
-
-def save_service(
-    user_id,
-    username,
-    subscription
-):
-
-    users = load_users()
-
-
-    users[str(user_id)] = {
-
+    services[user_id] = {
         "username": username,
-
         "subscription": subscription
-
     }
-
-
-    save_users(users)
-
-
-
 
 
 def get_service(user_id):
 
-    users = load_users()
-
-
-    return users.get(
-        str(user_id)
-    )
-
-
-
+    return services.get(user_id)
 
 
 # =========================
-# سفارش ها
+# سفارش‌ها
 # =========================
 
-def load_orders():
-
-    if not os.path.exists(ORDERS_FILE):
-
-        return {}
+orders = {}
 
 
-    with open(
-        ORDERS_FILE,
-        "r",
-        encoding="utf-8"
-    ) as f:
+def save_order(order_id, user_id, plan):
 
-        return json.load(f)
-
-
-
-
-
-def save_orders(orders):
-
-    with open(
-        ORDERS_FILE,
-        "w",
-        encoding="utf-8"
-    ) as f:
-
-        json.dump(
-            orders,
-            f,
-            ensure_ascii=False,
-            indent=4
-        )
-
-
-
-
-
-def save_order(
-    order_id,
-    user_id,
-    plan
-):
-
-    orders = load_orders()
-
-
-    orders[str(order_id)] = {
-
+    orders[order_id] = {
         "order_id": order_id,
-
         "user_id": user_id,
-
         "plan": plan
-
     }
 
 
-    save_orders(orders)
+def get_order(order_id):
+
+    return orders.get(order_id)
 
 
-
-
-
-def get_order(value):
-
-    orders = load_orders()
-
-
-    # پیدا کردن با شماره سفارش
-
-    if str(value) in orders:
-
-        return orders[str(value)]
-
-
-
-    # پیدا کردن با آیدی کاربر
+def get_order_by_user(user_id):
 
     for order in orders.values():
 
-        if order["user_id"] == value:
+        if order["user_id"] == user_id:
 
             return order
-
-
 
     return None
 
 
-
-
-
 def delete_order(order_id):
 
-    orders = load_orders()
+    if order_id in orders:
 
-
-    if str(order_id) in orders:
-
-        del orders[str(order_id)]
-
-
-    save_orders(orders)
+        del orders[order_id]
