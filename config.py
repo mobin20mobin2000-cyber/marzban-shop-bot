ae581d1b191c3d0c6f68c7cecca5dbaba82bf51e
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-22 12:21:49 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+MARZBAN_URL = os.getenv("MARZBAN_URL")
+
+MARZBAN_USERNAME = os.getenv("MARZBAN_USERNAME")
+
+MARZBAN_PASSWORD = os.getenv("MARZBAN_PASSWORD")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
