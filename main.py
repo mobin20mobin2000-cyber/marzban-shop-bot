ee1cbf90afdbeba37ef818047a5480bc41ca9452
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-24 14:20:43 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Zeus Shop VPN
 # =========================
 
+
 from telegram.ext import Application
 
 from config import BOT_TOKEN
@@ -12,28 +13,55 @@ from handlers import register_handlers
 from database import init_db
 
 
+
+# =========================
+# اجرای ربات
+# =========================
+
 def main():
 
-    # ساخت دیتابیس
+
+    # ساخت دیتابیس و جدول‌ها
+
     init_db()
 
 
+
+    # ساخت ربات
+
     app = Application.builder().token(
+
         BOT_TOKEN
+
     ).build()
 
 
-    register_handlers(app)
+
+    # ثبت Handler ها
+
+    register_handlers(
+
+        app
+
+    )
+
 
 
     print(
-        "🚀 Zeus Shop VPN Started"
+        "🚀 Zeus Shop VPN Bot Started"
     )
 
+
+
+    # اجرای ربات
 
     app.run_polling()
 
 
+
+# =========================
+# شروع
+# =========================
 
 if __name__ == "__main__":
 
