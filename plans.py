3c7fbd0e597b24f9731719186f5d9c819865ff1b
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-24 15:07:31 +0330

    Update plans.py

--- plans.py
+++ plans.py
@@ -3,36 +3,68 @@
 # Zeus Shop VPN
 # =========================
 
+
 PLANS = {
 
+
     "10GB": {
+
         "name": "۱۰ گیگ",
+
         "volume": 10 * 1024 * 1024 * 1024,
+
+        "days": 30,
+
         "price": 10000
+
     },
+
 
     "20GB": {
+
         "name": "۲۰ گیگ",
+
         "volume": 20 * 1024 * 1024 * 1024,
+
+        "days": 30,
+
         "price": 20000
+
     },
+
 
     "50GB": {
+
         "name": "۵۰ گیگ",
+
         "volume": 50 * 1024 * 1024 * 1024,
+
+        "days": 30,
+
         "price": 50000
+
     },
 
+
     "100GB": {
+
         "name": "۱۰۰ گیگ",
+
         "volume": 100 * 1024 * 1024 * 1024,
+
+        "days": 30,
+
         "price": 100000
+
     }
 
 }
 
 
 
+
 def get_plan(plan_id):
 
-    return PLANS.get(plan_id)
+    return PLANS.get(
+        plan_id
+    )
