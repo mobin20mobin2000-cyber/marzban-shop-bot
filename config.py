e3c4b70e98304d46de2b9ac0bb3d1004744d5396
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-24 15:04:36 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,31 +5,61 @@
 
 import os
 
+
 # =========================
 # Telegram
 # =========================
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_TOKEN = os.getenv(
+    "BOT_TOKEN"
+)
 
 ADMIN_ID = int(
-    os.getenv("ADMIN_ID", "0")
+    os.getenv(
+        "ADMIN_ID",
+        0
+    )
 )
+
+
 
 # =========================
 # Marzban
 # =========================
 
 MARZBAN_URL = os.getenv(
-    "MARZBAN_URL",
-    ""
+    "MARZBAN_URL"
 )
 
 MARZBAN_USERNAME = os.getenv(
-    "MARZBAN_USERNAME",
-    ""
+    "MARZBAN_USERNAME"
 )
 
 MARZBAN_PASSWORD = os.getenv(
-    "MARZBAN_PASSWORD",
-    ""
+    "MARZBAN_PASSWORD"
 )
+
+
+
+# =========================
+# بررسی تنظیمات
+# =========================
+
+if not BOT_TOKEN:
+    print("❌ BOT_TOKEN تنظیم نشده")
+
+
+if not ADMIN_ID:
+    print("❌ ADMIN_ID تنظیم نشده")
+
+
+if not MARZBAN_URL:
+    print("❌ MARZBAN_URL تنظیم نشده")
+
+
+if not MARZBAN_USERNAME:
+    print("❌ MARZBAN_USERNAME تنظیم نشده")
+
+
+if not MARZBAN_PASSWORD:
+    print("❌ MARZBAN_PASSWORD تنظیم نشده")
