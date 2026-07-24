a69bcb681b35b3ad66700a0b261ec46aa7081d3a
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-24 13:47:44 +0330

    Update order.py

--- order.py
+++ order.py
@@ -1,70 +1,24 @@
+# =========================
+# order.py
+# Zeus Shop VPN
+# =========================
+
 import time
 
 
-orders = {}
 
+# =========================
+# ساخت شماره سفارش
+# =========================
 
-def create_order(user_id, plan):
+def create_order(
+    user_id,
+    plan_name
+):
 
     order_id = str(
         int(time.time())
     )
 
-    orders[order_id] = {
-        "user_id": user_id,
-        "plan": plan,
-        "status": "waiting"
-    }
 
     return order_id
-
-
-
-def get_order(order_id):
-
-    return orders.get(
-        order_id
-    )
-
-
-
-def approve_order(order_id):
-
-    if order_id in orders:
-
-        orders[order_id]["status"] = "approved"
-
-        return True
-
-    return False
-
-
-
-def reject_order(order_id):
-
-    if order_id in orders:
-
-        orders[order_id]["status"] = "rejected"
-
-        return True
-
-    return False
-
-
-
-def user_orders(user_id):
-
-    result = []
-
-    for order_id, data in orders.items():
-
-        if data["user_id"] == user_id:
-
-            result.append(
-                {
-                    "id": order_id,
-                    **data
-                }
-            )
-
-    return result
