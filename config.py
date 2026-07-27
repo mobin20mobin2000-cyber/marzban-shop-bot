b5414ba3e83e485cbeb297c792b8be6995643510
mobin20mobin2000-cyber <mobin20mobin2000@gmail.com>
2026-07-27 12:15:29 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,25 +4,19 @@
 # ==========================================================
 
 
-# توکن ربات
 BOT_TOKEN = "8709572517:AAFK3-EPY_dAI7tjGm2wh_26aey5wIJo4PU"
 
 
-
-# آیدی ادمین
-# عدد خودت را بگذار
 ADMIN_ID = 5769833164
 
 
-
-# شماره کارت
-CARD_NUMBER = "6219-8619-8557-1842"
+CARD_NUMBER = "6219861985571842"
 
 
 
 # Marzban
 
-"MARZBAN_URL = "https://marzban-panel-production-9523.up.railway.app""
+MARZBAN_URL = "https://marzban-panel-production-9523.up.railway.app"
 
 MARZBAN_USERNAME = "mobin"
 
@@ -30,19 +24,16 @@ MARZBAN_PASSWORD = "009900"
 
 
 
-# کانال
-
 CHANNEL = "@Vpn1_v2rayNG"
 
 
 
-# قیمت‌ها
-
 PLANS = {
+
 
     "plan1": {
 
-        "name": "1 ماهه",
+        "name": "1 ماهه 20 گیگ",
 
         "volume": 50,
 
@@ -55,27 +46,28 @@ PLANS = {
 
     "plan2": {
 
-        "name": "1 ماهه",
+        "name": "1 ماهه 100 گیگ",
 
         "volume": 100,
 
         "days": 30,
 
-        "price": 140000
+        "price": 170000
 
     },
 
 
     "plan3": {
 
-        "name": "1 ماهه",
+        "name": "1 ماهه نامحدود",
 
-        "volume": نامحدود,
+        "volume": "نامحدود",
 
         "days": 30,
 
         "price": 165000
 
     }
+
 
 }
